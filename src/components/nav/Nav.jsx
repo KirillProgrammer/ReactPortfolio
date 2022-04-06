16bc8c4194dd76f37ff5a7e96977996fad0fb910
Kirill Karpov <kirillkarpovprogramm@gmail.com>
2022-04-06 22:00:10 +0300

    add icons in navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
 import MainScreen from '../main/MainScreen';
 import Projects from '../projects/Projects';
 import Contacts from '../contact/Contacts';
+import { RiHome3Line } from 'react-icons/ri';
+import { AiOutlineCloseSquare } from "react-icons/ai";
 import './nav.scss';
 
 function Nav(props) {
@@ -13,9 +15,9 @@ function Nav(props) {
   return (
     <BrowserRouter>
       <nav className={ hamburger ? 'nav-is-active' : '' }>
-        <span onClick={handleHamburger}></span>
-        <span onClick={handleHamburger}></span>
-        <span onClick={handleHamburger}></span>
+        <span onClick={handleHamburger}>
+          {hamburger ? <AiOutlineCloseSquare /> : <RiHome3Line /> }
+        </span>
         <NavLink
           to='/'
           onClick={handleHamburger}
